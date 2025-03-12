27ded54cab9ce5e09e9e35c67db246876d9d92dc
Victor Guerra Veloso <victorgvbh@gmail.com>
2025-03-11 22:47:54 -0400

    Harden UI against malformed DB users' data

--- webapp/src/model/user.ts
+++ webapp/src/model/user.ts
@@ -1,6 +1,16 @@
 export class User {
-    
-    constructor(private userID, private userName, private name: string, private familyName: string, private role: string, private email: string, private jwt: string) {}
+
+    constructor(private userID, private userName, private name: string, private familyName: string, private role: string, private email: string, private jwt: string) {
+      if (!this.name.length && this.userName.length > 0) {
+        this.name = this.userName;
+      }
+      if (!this.familyName.length) {
+        this.familyName = "";
+      }
+      if (!this.role.length) {
+        this.role = "USER";
+      }
+    }
 
     public getUserID() { return this.userID; }
     public getUserName() { return this.userName; }
@@ -10,5 +20,5 @@ export class User {
     public getJWT() { return this.jwt; }
 
     public isAdmin() { return this.role.toUpperCase() == "ADMIN"; }
-   
+
 }
